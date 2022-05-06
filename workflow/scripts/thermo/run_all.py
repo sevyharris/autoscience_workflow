32ca2a08534d77ca8ff4ddc88633d5b088313fd3
Sevy Harris <harris.se@northeastern.edu>
2022-05-06 12:28:07 -0400

    add refactored scripts for running all thermo

---
+++ workflow/scripts/thermo/run_all.py
@@ -0,0 +1,33 @@
+# Script for running a thermo job for one species
+import sys
+import job
+
+
+logfile = 'all_logs.txt'
+n_species = job.get_num_species()
+print(n_species)
+# skip_indices = [1, 2, 3, 4, 9, 10, 42, 45, 46, 47]
+skip_indices = [9, 10]
+for species_index in range(50, n_species + 1):
+    species_smiles = job.index2smiles(species_index)
+    if species_index in skip_indices:
+        with open(logfile, 'a') as f:
+            f.write(f'Skipping species {species_index}: {species_smiles}' + '\n')
+        print(f'Skipping species {species_index}: {species_smiles}')
+        continue
+
+    if job.arkane_complete(species_index):
+        with open(logfile, 'a') as f:
+            f.write(f"SPECIES {species_index}: {species_smiles} COMPLETE" + '\n')
+        print(f"SPECIES {species_index}: {species_smiles} COMPLETE")
+        continue
+
+    with open(logfile, 'a') as f:
+        f.write(f"Running Calculation for Species {species_index}: {species_smiles}" + '\n')
+    print(f"Running Calculation for Species {species_index}: {species_smiles}")
+    job.run_conformers_job(species_index)
+    job.run_rotors_job(species_index)
+    job.run_arkane_job(species_index)
+    with open(logfile, 'a') as f:
+        f.write(f"SPECIES {species_index}: {species_smiles} COMPLETE" + '\n')
+    print(f"SPECIES {species_index}: {species_smiles} COMPLETE")
---
+++ workflow/scripts/thermo/run_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=all_thermo
+#SBATCH --error=error.log
+#SBATCH --output=output.log
+#SBATCH --partition=west
+#SBATCH --time=7-00:00:00
+#SBATCH --mincpus=1
+#SBATCH --exclude=c5003
+
+
+cd "/work/westgroup/harris.se/autoscience/autoscience_workflow/workflow/"
+python "/work/westgroup/harris.se/autoscience/autoscience_workflow/workflow/scripts/thermo/run_all.py"

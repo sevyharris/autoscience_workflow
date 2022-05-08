5320daa2272490b7ed7640a7f022d0231a670e49
Sevy Harris <harris.se@northeastern.edu>
2022-05-08 15:17:42 -0400

    add reverse direction for TS search

--- workflow/scripts/kinetics/kineticfun.py
+++ workflow/scripts/kinetics/kineticfun.py
@@ -65,7 +65,7 @@ def termination_status(log_file):
         return -1
 
 
-def run_TS_shell_calc(reaction_index):
+def run_TS_shell_calc(reaction_index, use_reverse=False):
     """Start an optimization keeping the reaction center fixed
     """
     reaction_base_dir = os.path.join(DFT_DIR, 'kinetics', f'reaction_{reaction_index:04}')
@@ -75,7 +75,13 @@ def run_TS_shell_calc(reaction_index):
     logfile = os.path.join(shell_dir, 'shell.log')
 
     # Check for already finished shell logfile
-    shell_opt_log = os.path.join(shell_dir, 'fwd_ts_0000.log')
+    shell_label = 'fwd_ts_0000.log'
+    direction = 'forward'
+    if use_reverse:
+        shell_label = 'rev_ts_0000.log'
+        direction = 'reverse'
+    shell_opt_log = os.path.join(shell_dir, shell_label)
+
     if os.path.exists(shell_opt_log):
         status = termination_status(shell_opt_log)
         if status == 0 or status == 1:
@@ -86,79 +92,79 @@ def run_TS_shell_calc(reaction_index):
 
     reaction_smiles = reaction_index2smiles(reaction_index)
     reaction = autotst.reaction.Reaction(label=reaction_smiles)
-    reaction.ts['forward'][0].get_molecules()
+    reaction.ts[direction][0].get_molecules()
     reaction.generate_conformers(ase_calculator=Hotbit())
 
     # Do the shell calculation
     # write Gaussian input files
-    if len(reaction.ts['forward']) > 1:
-        raise ValueError('multiple forward ts')
+    if len(reaction.ts[direction]) > 1:
+        raise ValueError(f'multiple {direction} ts')
 
-    for i, ts in enumerate(reaction.ts['forward']):
-        gaussian = autotst.calculator.gaussian.Gaussian(conformer=ts)
-        calc = gaussian.get_shell_calc()
-        calc.label = f'fwd_ts_{i:04}'
-        calc.directory = shell_dir
-        calc.parameters.pop('scratch')
-        calc.parameters.pop('multiplicity')
-        calc.parameters['mult'] = ts.rmg_molecule.multiplicity
-        calc.write_input(ts.ase_molecule)
+    ts = reaction.ts[direction][0]
+    gaussian = autotst.calculator.gaussian.Gaussian(conformer=ts)
+    calc = gaussian.get_shell_calc()
+    calc.label = shell_label[:-4]
+    calc.directory = shell_dir
+    calc.parameters.pop('scratch')
+    calc.parameters.pop('multiplicity')
+    calc.parameters['mult'] = ts.rmg_molecule.multiplicity
+    calc.write_input(ts.ase_molecule)
 
-        # Get rid of double-space between xyz block and mod-redundant section
-        double_space = False
-        lines = []
-        with open(os.path.join(shell_dir, calc.label + '.com'), 'r') as f:
-            lines = f.readlines()
-            for j in range(1, len(lines)):
-                if lines[j] == '\n' and lines[j - 1] == '\n':
-                    double_space = True
-                    break
-        if double_space:
-            lines = lines[0:j - 1] + lines[j:]
-            with open(os.path.join(shell_dir, calc.label + '.com'), 'w') as f:
-                f.writelines(lines)
+    # Get rid of double-space between xyz block and mod-redundant section
+    double_space = False
+    lines = []
+    with open(os.path.join(shell_dir, calc.label + '.com'), 'r') as f:
+        lines = f.readlines()
+        for j in range(1, len(lines)):
+            if lines[j] == '\n' and lines[j - 1] == '\n':
+                double_space = True
+                break
+    if double_space:
+        lines = lines[0:j - 1] + lines[j:]
+        with open(os.path.join(shell_dir, calc.label + '.com'), 'w') as f:
+            f.writelines(lines)
 
-        # make the shell slurm script
-        slurm_run_file = os.path.join(shell_dir, 'run.sh')
-        slurm_settings = {
-            '--job-name': f'g16_shell_{reaction_index}',
-            '--error': 'error.log',
-            '--output': 'output.log',
-            '--nodes': 1,
-            '--partition': 'west,short',
-            '--exclude': 'c5003',
-            '--mem': '20Gb',
-            '--time': '24:00:00',
-            '--cpus-per-task': 16,
-            # TODO make this an array if multiple forward ts's
-        }
-        slurm_file_writer = job_manager.SlurmJobFile(full_path=slurm_run_file)
-        slurm_file_writer.settings = slurm_settings
-        slurm_file_writer.content = [
-            'export GAUSS_SCRDIR=/scratch/harris.se/guassian_scratch\n',
-            'mkdir -p $GAUSS_SCRDIR\n',
-            'module load gaussian/g16\n',
-            'source /shared/centos7/gaussian/g16/bsd/g16.profile\n\n',
+    # make the shell slurm script
+    slurm_run_file = os.path.join(shell_dir, 'run.sh')
+    slurm_settings = {
+        '--job-name': f'g16_shell_{reaction_index}',
+        '--error': 'error.log',
+        '--output': 'output.log',
+        '--nodes': 1,
+        '--partition': 'west,short',
+        '--exclude': 'c5003',
+        '--mem': '20Gb',
+        '--time': '24:00:00',
+        '--cpus-per-task': 16,
+        # TODO make this an array if multiple forward ts's
+    }
+    slurm_file_writer = job_manager.SlurmJobFile(full_path=slurm_run_file)
+    slurm_file_writer.settings = slurm_settings
+    slurm_file_writer.content = [
+        'export GAUSS_SCRDIR=/scratch/harris.se/guassian_scratch\n',
+        'mkdir -p $GAUSS_SCRDIR\n',
+        'module load gaussian/g16\n',
+        'source /shared/centos7/gaussian/g16/bsd/g16.profile\n\n',
 
-            f'g16 fwd_ts_{i:04}.com\n',
-        ]
-        slurm_file_writer.write_file()
+        f'g16 {shell_label[:-4]}.com' + '\n',
+    ]
+    slurm_file_writer.write_file()
 
-        # submit the job
-        with open(logfile, 'a') as f:
-            f.write('Submitting shell optimization job\n')
-        start_dir = os.getcwd()
-        os.chdir(shell_dir)
-        shell_job = job_manager.SlurmJob()
-        slurm_cmd = f"sbatch {slurm_run_file}"
-        shell_job.submit(slurm_cmd)
-        os.chdir(start_dir)
-        shell_job.wait_all(check_interval=600)
+    # submit the job
+    with open(logfile, 'a') as f:
+        f.write('Submitting shell optimization job\n')
+    start_dir = os.getcwd()
+    os.chdir(shell_dir)
+    shell_job = job_manager.SlurmJob()
+    slurm_cmd = f"sbatch {slurm_run_file}"
+    shell_job.submit(slurm_cmd)
+    os.chdir(start_dir)
+    shell_job.wait(check_interval=600)
 
-        # TODO restart optimization if it fails. for now, move on
+    # TODO restart optimization if it fails. for now, move on
 
 
-def run_TS_overall_calc(reaction_index):
+def run_TS_overall_calc(reaction_index, use_reverse=False):
     """Start a TS optimization from the geometry of the shell calculation
     """
     reaction_base_dir = os.path.join(DFT_DIR, 'kinetics', f'reaction_{reaction_index:04}')
@@ -168,8 +174,14 @@ def run_TS_overall_calc(reaction_index):
     overall_dir = os.path.join(reaction_base_dir, 'overall')
     os.makedirs(overall_dir, exist_ok=True)
 
+    overall_label = 'fwd_ts_0000.log'
+    direction = 'forward'
+    if use_reverse:
+        overall_label = 'rev_ts_0000.log'
+        direction = 'reverse'
+    overall_ts_log = os.path.join(overall_dir, overall_label)
+
     # check that the overall job hasn't finished
-    overall_ts_log = os.path.join(overall_dir, 'fwd_ts_0000.log')
     if os.path.exists(overall_ts_log):
         status = termination_status(overall_ts_log)
         if status == 0:
@@ -180,26 +192,26 @@ def run_TS_overall_calc(reaction_index):
 
     reaction_smiles = reaction_index2smiles(reaction_index)
     reaction = autotst.reaction.Reaction(label=reaction_smiles)
-    reaction.ts['forward'][0].get_molecules()
+    reaction.ts[direction][0].get_molecules()
     reaction.generate_conformers(ase_calculator=Hotbit())
 
-    if len(reaction.ts['forward']) > 1:
-        raise ValueError('multiple forward ts')
+    if len(reaction.ts[direction]) > 1:
+        raise ValueError(f'multiple {direction} ts')
 
-    shell_opt = os.path.join(shell_dir, 'fwd_ts_0000.log')
+    shell_opt = os.path.join(shell_dir, overall_label)
     try:
         with open(shell_opt, 'r') as f:
             atoms = ase.io.gaussian.read_gaussian_out(f)
-            reaction.ts['forward'][0]._ase_molecule = atoms
+            reaction.ts[direction][0]._ase_molecule = atoms
     except IndexError:
         # handle case where all degrees of freedom were frozen in the shell calculation
-        if len(reaction.ts['forward'][0]._ase_molecule) > 3:
+        if len(reaction.ts[direction][0]._ase_molecule) > 3:
             raise ValueError('Shell optimization failed to converge. Rerun it!')
 
-    for i, ts in enumerate(reaction.ts['forward']):
+    for i, ts in enumerate(reaction.ts[direction]):
         gaussian = autotst.calculator.gaussian.Gaussian(conformer=ts)
         calc = gaussian.get_overall_calc()
-        calc.label = f'fwd_ts_{i:04}'
+        calc.label = overall_label[:-4]
         calc.directory = overall_dir
         calc.parameters.pop('scratch')
         calc.parameters.pop('multiplicity')
@@ -243,7 +255,7 @@ def run_TS_overall_calc(reaction_index):
             'module load gaussian/g16\n',
             'source /shared/centos7/gaussian/g16/bsd/g16.profile\n\n',
 
-            f'g16 fwd_ts_{i:04}.com\n',
+            f'g16 {overall_label[:-4]}.com' + '\n',
         ]
         slurm_file_writer.write_file()
 
@@ -254,3 +266,4 @@ def run_TS_overall_calc(reaction_index):
         slurm_cmd = f"sbatch {slurm_run_file}"
         overall_job.submit(slurm_cmd)
         os.chdir(start_dir)
+        overall_job.wait(check_interval=600)

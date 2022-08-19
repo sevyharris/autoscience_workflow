ac399bf100bdc240cf5ce251386b0c0fa1c24b41
Sevy Harris <harris.se@northeastern.edu>
2022-08-19 10:31:52 -0400

    add option to handle multiple forward TS

--- workflow/scripts/kinetics/kineticfun.py
+++ workflow/scripts/kinetics/kineticfun.py
@@ -96,73 +96,72 @@ def run_TS_shell_calc(reaction_index, use_reverse=False):
     reaction.ts[direction][0].get_molecules()
     reaction.generate_conformers(ase_calculator=Hotbit())
 
-    # Do the shell calculation
+    # Do the shell calculations
     # write Gaussian input files
-    if len(reaction.ts[direction]) > 1:
-        raise ValueError(f'multiple {direction} ts')
+    for i in range(0, len(reaction.ts[direction])):
 
-    ts = reaction.ts[direction][0]
-    gaussian = autotst.calculator.gaussian.Gaussian(conformer=ts)
-    calc = gaussian.get_shell_calc()
-    calc.label = shell_label[:-4]
-    calc.directory = shell_dir
-    calc.parameters.pop('scratch')
-    calc.parameters.pop('multiplicity')
-    calc.parameters['mult'] = ts.rmg_molecule.multiplicity
-    calc.write_input(ts.ase_molecule)
+        ts = reaction.ts[direction][i]
+        gaussian = autotst.calculator.gaussian.Gaussian(conformer=ts)
+        calc = gaussian.get_shell_calc()
+        calc.label = shell_label[:-4] + f'_{i:04}'
+        calc.directory = shell_dir
+        calc.parameters.pop('scratch')
+        calc.parameters.pop('multiplicity')
+        calc.parameters['mult'] = ts.rmg_molecule.multiplicity
+        calc.write_input(ts.ase_molecule)
 
-    # Get rid of double-space between xyz block and mod-redundant section
-    double_space = False
-    lines = []
-    with open(os.path.join(shell_dir, calc.label + '.com'), 'r') as f:
-        lines = f.readlines()
-        for j in range(1, len(lines)):
-            if lines[j] == '\n' and lines[j - 1] == '\n':
-                double_space = True
-                break
-    if double_space:
-        lines = lines[0:j - 1] + lines[j:]
-        with open(os.path.join(shell_dir, calc.label + '.com'), 'w') as f:
-            f.writelines(lines)
+        # Get rid of double-space between xyz block and mod-redundant section
+        double_space = False
+        lines = []
+        with open(os.path.join(shell_dir, calc.label + '.com'), 'r') as f:
+            lines = f.readlines()
+            for j in range(1, len(lines)):
+                if lines[j] == '\n' and lines[j - 1] == '\n':
+                    double_space = True
+                    break
+        if double_space:
+            lines = lines[0:j - 1] + lines[j:]
+            with open(os.path.join(shell_dir, calc.label + '.com'), 'w') as f:
+                f.writelines(lines)
 
-    # make the shell slurm script
-    slurm_run_file = os.path.join(shell_dir, 'run.sh')
-    slurm_settings = {
-        '--job-name': f'g16_shell_{reaction_index}',
-        '--error': 'error.log',
-        '--output': 'output.log',
-        '--nodes': 1,
-        '--partition': 'west,short',
-        '--exclude': 'c5003',
-        '--mem': '20Gb',
-        '--time': '24:00:00',
-        '--cpus-per-task': 16,
-        # TODO make this an array if multiple forward ts's
-    }
-    slurm_file_writer = job_manager.SlurmJobFile(full_path=slurm_run_file)
-    slurm_file_writer.settings = slurm_settings
-    slurm_file_writer.content = [
-        'export GAUSS_SCRDIR=/scratch/harris.se/guassian_scratch\n',
-        'mkdir -p $GAUSS_SCRDIR\n',
-        'module load gaussian/g16\n',
-        'source /shared/centos7/gaussian/g16/bsd/g16.profile\n\n',
+        # make the shell slurm script
+        slurm_run_file = os.path.join(shell_dir, f'run_{i:04}.sh')
+        slurm_settings = {
+            '--job-name': f'g16_shell_{reaction_index}',
+            '--error': 'error.log',
+            '--output': 'output.log',
+            '--nodes': 1,
+            '--partition': 'west,short',
+            '--exclude': 'c5003',
+            '--mem': '20Gb',
+            '--time': '24:00:00',
+            '--cpus-per-task': 16,
+            # TODO make this an array if multiple forward ts's
+        }
+        slurm_file_writer = job_manager.SlurmJobFile(full_path=slurm_run_file)
+        slurm_file_writer.settings = slurm_settings
+        slurm_file_writer.content = [
+            'export GAUSS_SCRDIR=/scratch/harris.se/guassian_scratch\n',
+            'mkdir -p $GAUSS_SCRDIR\n',
+            'module load gaussian/g16\n',
+            'source /shared/centos7/gaussian/g16/bsd/g16.profile\n\n',
 
-        f'g16 {shell_label[:-4]}.com' + '\n',
-    ]
-    slurm_file_writer.write_file()
+            f'g16 {shell_label[:-4]}_{i:04}.com' + '\n',
+        ]
+        slurm_file_writer.write_file()
 
-    # submit the job
-    with open(logfile, 'a') as f:
-        f.write('Submitting shell optimization job\n')
-    start_dir = os.getcwd()
-    os.chdir(shell_dir)
-    shell_job = job_manager.SlurmJob()
-    slurm_cmd = f"sbatch {slurm_run_file}"
-    shell_job.submit(slurm_cmd)
-    os.chdir(start_dir)
-    shell_job.wait(check_interval=600)
+        # submit the job
+        with open(logfile, 'a') as f:
+            f.write('Submitting shell optimization job\n')
+        start_dir = os.getcwd()
+        os.chdir(shell_dir)
+        shell_job = job_manager.SlurmJob()
+        slurm_cmd = f"sbatch {slurm_run_file}"
+        shell_job.submit(slurm_cmd)
+        os.chdir(start_dir)
+        shell_job.wait(check_interval=600)
 
-    # TODO restart optimization if it fails. for now, move on
+        # TODO restart optimization if it fails. for now, move on
 
 
 def run_TS_overall_calc(reaction_index, use_reverse=False):
--- workflow/scripts/kinetics/run_one.sh
+++ workflow/scripts/kinetics/run_one.sh
@@ -3,8 +3,8 @@
 #SBATCH --error=kinetics_error.log
 #SBATCH --mincpus=1
 #SBATCH --exclude=c5003
-#SBATCH --partition=west,short
-#SBATCH --time=1-00:00:00
+#SBATCH --partition=west,short,express
+#SBATCH --time=00:30:00
 
 
 cd "/work/westgroup/harris.se/autoscience/autoscience_workflow/workflow/"
